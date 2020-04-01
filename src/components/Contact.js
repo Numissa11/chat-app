490cd42eab263e9eba806b8f548e566d744e35d5
Numissa11 <aline_sinclaire@hotmail.fr>
2020-04-01 13:42:16 +0100

    project finished

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Contact.css';
+   
+function Contact(props) {
+    return (
+    <div className="Contact">
+         <img className="avatar" src={props.avatar} alt={props.name}/>
+        <div>
+            <h1 className="name">{props.name}</h1>
+            <div className="status">
+            <div className= {props.isOnline ? "status-online" : "status-offline"}>
+            </div>
+            <p className="status-text">{props.isOnline ? "online" : "offline"}</p>
+            </div>
+        </div>
+    
+    </div>
+    )
+}
+
+export default Contact
