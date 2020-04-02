5191059686ce80764e8ae6ed9b7ac4c93f9dcc2b
Numissa11 <aline_sinclaire@hotmail.fr>
2020-04-02 01:03:40 +0100

    commit my final version of quest 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Contact from './components/Contact';
 
-const name = "Camillus Victorius";
+
+const name = "Camille Ramiere";
 const avatar = "https://randomuser.me/api/portraits/women/65.jpg"
 
+const name2 = "Alexandre De Margerie"
+const avatar2 = "https://randomuser.me/api/portraits/men/47.jpg"
+const isOnline = false
+
+const name3 = "Hermina Krasznai"
+const avatar3 = "https://randomuser.me/api/portraits/women/46.jpg"
 function App() {
   return (
     <div className="App">
-            <Contact
+       <Contact
          name={name}
          isOnline
          avatar={avatar}
       />
+      <Contact
+         name={name2}
+         isOnline = {isOnline}
+         avatar={avatar2}
+      />
+    <Contact
+         name={name3}
+         isOnline
+         avatar={avatar3}
+      />
     </div>
   );
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar : PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
 
 export default App;
